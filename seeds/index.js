8793524e57f2fd1211f8f149e10f671f7fc41f96
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-12 18:32:26 +0000

    Bootstrap basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -16,10 +16,14 @@ const seedDb = async () => {
 
     for (let i = 0; i < numCampgrounds; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://source.unsplash.com/random/300x300?camping,${i}`,
+            description: "Lorem Ipsum Dolor Sit Amet",
+            price: price
         })
 
         await camp.save();
