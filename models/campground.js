e2379974ed3b6da9d6acfb46fff89909bd26ce33
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-17 15:56:14 +0000

    Adding Reviews

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,29 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const CampgroundSchema = new Schema({
     title: String,
     price: Number,
     description: String,
     location: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
+})
+
+CampgroundSchema.post("findOneAndDelete", async function (campground) {
+    if (campground) {
+        await Review.deleteMany({
+            _id: {
+                $in: campground.reviews
+            }
+        })
+    }
 })
 
 const Campground = mongoose.model("Campground", CampgroundSchema);
