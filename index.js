c18cbda7fea78ebc3f1a016e5233510f01f500c2
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-19 01:52:41 +0000

    Session and Flash

--- index.js
+++ index.js
@@ -12,21 +12,43 @@ const { campgroundSchema, reviewSchema } = require("./schemas");
 const Review = require("./models/review");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => console.log("Mongo connection open"))
     .catch(err => console.log(err))
+
+const sessionConfig = {
+    secret: "thisshouldbeabettersecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+}
 
 app.engine("ejs", ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
 app.use(express.urlencoded());
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sessionConfig));
+app.use(flash());
+// The flash needs to be defined beforet the routes
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
-app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get("/", (req, res) => {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,29 +27,42 @@ router.get("/new", (req, res) => {
 
 router.get("/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate("reviews");
-    res.render("campgrounds/show", { campground });
+    if (!campground) {
+        req.flash("error", "Cannot find that Campground.");
+        res.redirect("/campgrounds");
+    } else {
+        res.render("campgrounds/show", { campground });
+    }
 }))
 
 router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash("success", "Successfully added a Campground!");
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 router.get("/:id/edit", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render("campgrounds/edit", { campground });
+    if (!campground) {
+        req.flash("error", "Cannot find that Campground.");
+        res.redirect("/campgrounds");
+    } else {
+        res.render("campgrounds/edit", { campground });
+    }
 }))
 
 router.put("/:id", validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
+    req.flash("success", "Successfully edited a Campground!");
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 router.delete("/:id", catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted Campground!");
     res.redirect("/campgrounds");
 }))
 
