a50c5ee654b3164beb4ee3d31c47ca25a566973b
alexandrunst <nastase.alexandru100@gmail.com>
2023-02-21 16:38:40 +0000

    Routes refactoring

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,8 +4,6 @@ const catchAsync = require("../utils/catchAsync");
 const reviews = require("../controllers/reviews");
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
-
-
 router.post("/", isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 router.delete("/:reviewId", isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
